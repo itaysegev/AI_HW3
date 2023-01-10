2216ae8c2528758baa5005426e6c5ae01fdebc87
Itay Segev <itaysegev11@gmail.com>
2023-01-10 12:40:10 +0200

    policy iteration implementation

--- MDP/mdp_rl_implementation.py
+++ MDP/mdp_rl_implementation.py
@@ -7,6 +7,8 @@ from typing import List, Tuple, Dict
 def expectation_calc(p: Tuple[int, int, int, int], curr_i, curr_j, U, mdp):
     sum = 0
     curr_state = (curr_i, curr_j)
+    if curr_state in mdp.terminal_states:
+        return sum
     for k, action in enumerate(mdp.actions):
         next_state = mdp.step(curr_state, action)
         sum += p[k] * U[next_state[0]][next_state[1]]
@@ -57,7 +59,16 @@ def get_policy(mdp, U):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    policy = deepcopy(U)
+    for i in range(mdp.num_row):
+        for j in range(mdp.num_col):
+            if mdp.board[i][j] == "WALL":
+                continue
+            value_action_lst = [(expectation_calc(mdp.transition_function[action], i, j, U,
+                                 mdp), action) for action in mdp.actions]
+            policy[i][j] = max(value_action_lst, key=lambda tup: tup[0])[1]
+
+    return policy
     # ========================
 
 
@@ -94,6 +105,25 @@ def q_table_policy_extraction(mdp, qtable):
 
 # BONUS
 
+def p_value_calc(s, policy, mdp):
+    p_dict = {}
+    for i in range(mdp.num_row):
+        for j in range(mdp.num_col):
+            p_dict[(i, j)] = 0
+    P = np.zeros((mdp.num_row, mdp.num_col))
+    step = policy[s[0]][s[1]]
+    if step == 0 or step == 'WALL':
+        return P.flatten()
+    possible_states = [mdp.step(s, action) for action in mdp.actions]
+    for k in range(len(mdp.actions)):
+        p_dict[possible_states[k]] += float(mdp.transition_function[step][k])
+    for i in range(mdp.num_row):
+        for j in range(mdp.num_col):
+            P[i][j] = p_dict[(i, j)]
+    return P.flatten()
+
+
+
 def policy_evaluation(mdp, policy):
     # TODO:
     # Given the mdp, and a policy
@@ -101,7 +131,21 @@ def policy_evaluation(mdp, policy):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    P = np.zeros(mdp.num_row * mdp.num_col)
+    R = deepcopy(mdp.board)
+    for i in range(mdp.num_row):
+        for j in range(mdp.num_col):
+            P = np.concatenate((P, p_value_calc((i, j), policy, mdp)), axis=0)
+            if mdp.board[i][j] == "WALL":
+                R[i][j] = 0
+            else:
+                # P = np.concatenate((P, p_value_calc((i, j), policy, mdp)), axis=0)
+                R[i][j] = float(mdp.board[i][j])
+    R = np.array(R).flatten()
+    P = P.reshape((mdp.num_row * mdp.num_col + 1, mdp.num_row * mdp.num_col))[1:]
+    I = np.eye(12)
+    U = np.linalg.inv(I - mdp.gamma * P) @ R
+    return U.reshape((mdp.num_row, mdp.num_col))
     # ========================
 
 
@@ -113,5 +157,24 @@ def policy_iteration(mdp, policy_init):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    policy = deepcopy(policy_init)
+    while True:
+        U = policy_evaluation(mdp, policy)
+        unchanged = True
+        for i in range(mdp.num_row):
+            for j in range(mdp.num_col):
+                if mdp.board[i][j] == "WALL":
+                    continue
+                value_action_lst = [(expectation_calc(mdp.transition_function[action], i, j, U,
+                                                      mdp), action) for action in mdp.actions]
+                best_value, best_action = max(value_action_lst, key=lambda tup: tup[0])
+                policy_action = policy[i][j]
+                if policy_action == 0:
+                    continue
+                if best_value > expectation_calc(mdp.transition_function[policy_action], i, j, U, mdp):
+                    policy[i][j] = best_action
+                    unchanged = False
+        if unchanged:
+            return policy
+
     # ========================
