97bbf1101e1137ddc9bfafc9d6c815f981bbae67
Itay Segev <itaysegev11@gmail.com>
2023-01-13 13:23:21 +0200

    Q-learning implementation

--- MDP/mdp_rl_implementation.py
+++ MDP/mdp_rl_implementation.py
@@ -72,6 +72,21 @@ def get_policy(mdp, U):
     # ========================
 
 
+def outcome(state, curr_action, mdp):
+    action_lst = [action for action in mdp.actions]
+    weights = [100 * val for val in mdp.transition_function[curr_action]]
+    curr_action = random.choices(population=action_lst, weights=weights, k=1)[0]
+    new_state = mdp.step(state, curr_action)
+    reward = float(mdp.board[new_state[0]][new_state[1]])
+    done = new_state in mdp.terminal_states
+    return new_state, reward, done
+
+
+
+def q_table_state_format(state: Tuple[int,int], num_row):
+     return state[0] * (num_row + 1) + state[1]
+
+
 def q_learning(mdp, init_state, total_episodes=10000, max_steps=999, learning_rate=0.7, epsilon=1.0,
                       max_epsilon=1.0, min_epsilon=0.01, decay_rate=0.8):
     # TODO:
@@ -88,7 +103,55 @@ def q_learning(mdp, init_state, total_episodes=10000, max_steps=999, learning_ra
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    action_list = [action for action in mdp.actions]
+    action_size = len(action_list)
+    indexing = lambda action: action_list.index(action)
+    state_size = mdp.num_col * mdp.num_row
+    qtable = np.zeros((state_size, action_size))
+
+    for episode in range(total_episodes):
+        # Reset the environment
+        state = init_state
+        step = 0
+        done = False
+
+        for step in range(max_steps):
+            # Choose an action (a) in the current world state (s)
+
+            ## First we randomize a number
+            exp_exp_tradeoff = random.uniform(0, 1)
+            curr_q_index = q_table_state_format(state, mdp.num_row)
+            ## If this number > greater than epsilon --> exploitation (taking the biggest Q value for this state)
+            if exp_exp_tradeoff > epsilon:
+                action = action_list[np.argmax(qtable[curr_q_index, :])]
+
+            # Else doing a random choice --> exploration
+            else:
+                action = random.choice(action_list)
+
+
+            # Take the action (a) and observe the outcome state(s') and reward (r)
+
+            new_state, reward, done = outcome(state, action, mdp)
+
+            new_q_index = q_table_state_format(new_state, mdp.num_row)
+
+            action_index = indexing(action)
+            # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+            qtable[curr_q_index, action_index] = qtable[curr_q_index, action_index] + learning_rate * (reward + mdp.gamma *
+                                                                             np.max(qtable[new_q_index, :]) - qtable[
+                                                                                 curr_q_index, action_index])
+
+            # Our new state is state
+            state = new_state
+
+            # If done : finish episode
+            if done == True:
+                break
+
+        # Reduce epsilon (because we need less and less exploration)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+    return qtable
     # ========================
 
 
@@ -99,7 +162,15 @@ def q_table_policy_extraction(mdp, qtable):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    action_list = [action for action in mdp.actions]
+    policy = deepcopy(mdp.board)
+    for i in range(mdp.num_row):
+        for j in range(mdp.num_col):
+            if mdp.board[i][j] == "WALL":
+                continue
+            q_index = q_table_state_format((i, j), mdp.num_row)
+            policy[i][j] = action_list[np.argmax(qtable[q_index, :])]
+    return policy
     # ========================
 
 
